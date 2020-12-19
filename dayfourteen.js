2959dfbd77818a2d7b85db333c0b961da8990912
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-12-18 20:34:51 -0500

    Complete Day Fourteen Part Two

--- dayfourteen.js
+++ dayfourteen.js
@@ -18,6 +18,21 @@ function parseInput(input) {
   })
 }
 
+function parseInput2(input) {
+  return input.map(el => {
+    if (el.slice(0, 3) === 'mem') {
+      const memUpdate = el.split(' = ');
+      const value = parseInt(memUpdate[1])
+      const memSplit = memUpdate[0].split('[')[1];
+      const location = parseInt(memSplit.slice(0, memSplit.length - 1));
+      return {location, value}
+    } else {
+      const maskSplit = el.split(' = ');
+      return { mask: el.split(' = ')[1] }
+    }
+  })
+}
+
 function maskFlip(str) {
   return [...str].reverse().map((el, i) => {
     if (el === 'X') {
@@ -57,4 +72,32 @@ function partOne(input) {
   return mem.reduce((a, b) => a + b);
 }
 
+function partTwo(input) {
+  const operations = parseInput2(input);
+  let currentMask = null, mem = new Map();
+  operations.forEach(entry => {
+    if (entry.mask) {
+      currentMask = entry.mask;
+    } else {
+      let address = entry.location.toString(2).padStart(36, 0).split('');
+      address = address.map((bit, i) => {
+        if (currentMask[i] === '1') return 1;
+        if (currentMask[i] === 'X') return 'X'
+        else return bit;
+      }).join('');
+      const floaters = address.split('X');
+      let addresses = floaters.reduce((acc, floater) => {
+        if (acc.length === 0) return [floater];
+        const zeroes = [...acc].map(x => x + '0' + floater);
+        const ones = [...acc].map(x => x + '1' + floater);
+        return [...zeroes, ...ones];
+      }, []);
+      addresses.forEach(addr => mem.set(parseInt(addr, 2), entry.value));
+    }
+  })
+
+  return [...mem.values()].reduce((a, b) => a + b, 0);
+}
+
 console.log(partOne(input));
+console.log(partTwo(input));
